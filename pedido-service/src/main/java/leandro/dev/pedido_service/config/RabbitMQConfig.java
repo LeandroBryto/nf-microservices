e493683e6ead42590bd7b634a97ea64c5094924a
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-11 20:11:57 -0300

    feat:(config)RabbitMQConfig
    criação da configuração , a fila de pedidos , conversor de mensagens JSOM e o template do RabbitMQ para envio das mensagens

---
+++ pedido-service/src/main/java/leandro/dev/pedido_service/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package leandro.dev.pedido_service.config;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String PEDIDOS_CRIADOS_QUEUE = "pedidos.criados";
+
+    @Bean
+    public Queue pedidosCriadosQueue(){
+        return QueueBuilder.durable(PEDIDOS_CRIADOS_QUEUE).build();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
