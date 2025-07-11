bceb567499476cddba2907727d0bb82facd4e489
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-11 18:21:21 -0300

    feat(model):add Order entity
    criação para classe Order

---
+++ pedido-service/src/main/java/leandro/dev/pedido_service/model/Order.java
@@ -0,0 +1,38 @@
+package leandro.dev.pedido_service.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "pedidos")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "cliente_nome", nullable = false)
+    private String clienteNome;
+
+    @Column(name = "cliente_email", nullable = false)
+    private String clienteEmail;
+
+    @Column(name = "cliente_cpf", nullable = false , length = 14)
+    private String clienteCpf;
+
+    @Column(name = "valor", nullable = false , precision = 10,scale = 2)
+    private BigDecimal valor;
+
+    @Column(name = "descricao", columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(name = "status", nullable = false ,length = 50)
+    private String status = "Criado";
+
+    @CreationTimestamp
+    @Column(name =  "data_criacao", nullable = false)
+    private LocalDateTime dataCriacao;
+}
