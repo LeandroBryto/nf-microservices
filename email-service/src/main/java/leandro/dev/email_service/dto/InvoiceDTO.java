e46d4a440412459c1139c78a0502e79d88d3af45
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-17 17:26:37 -0300

    feat:(email-service(dto)InvoiceDTO

---
+++ email-service/src/main/java/leandro/dev/email_service/dto/InvoiceDTO.java
@@ -0,0 +1,28 @@
+package leandro.dev.email_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvoiceDTO {
+
+    private UUID id;
+    private String numeroNf;
+    private UUID saleId;
+    private UUID orderId;
+    private String clienteNome;
+    private String clienteEmail;
+    private String clienteCpf;
+    private BigDecimal valor;
+    private String descricao;
+    private LocalDateTime dataEmissao;
+    private String caminhoArquivoPdf;
+    private String status;
+}
