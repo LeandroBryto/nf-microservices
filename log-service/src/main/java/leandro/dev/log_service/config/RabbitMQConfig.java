224837badf2a4f10c40ffe1d4d11825c3119e1c3
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-18 21:05:22 -0300

    feat:(log-service(config)RabbitMQConfig
    adicionando configuração do RabbitMQ com fila de logs e conversor JSON

---
+++ log-service/src/main/java/leandro/dev/log_service/config/RabbitMQConfig.java
@@ -0,0 +1,33 @@
+package leandro.dev.log_service.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+
+    public static final String LOGS_QUEUE = "logs";
+    
+    @Bean
+    public Queue logsQueue(){
+        return QueueBuilder.durable(LOGS_QUEUE).build();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
